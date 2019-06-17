105351e9a49c7b8c51ec4d8260005afd415a9141
davidwkaiser <davidwkaiser@yahoo.com>
2019-06-16 19:57:40 -0500

    first commit, second time

---
+++ src/main/kotlin/com/example/tarot/TarotApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tarot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TarotApplication
+
+fun main(args: Array<String>) {
+	runApplication<TarotApplication>(*args)
+}
