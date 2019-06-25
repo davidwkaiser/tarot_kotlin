5dcec966691e7e063ed9deb8b1b176008adba7be
davidwkaiser <davidwkaiser@yahoo.com>
2019-06-24 21:53:21 -0500

    add thymeleaf to gradle, add index.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 //	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.3.1")
